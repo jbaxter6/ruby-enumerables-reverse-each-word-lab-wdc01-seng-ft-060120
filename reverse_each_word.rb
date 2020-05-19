d7ef1610481e1d4afa9c764d528c4ce47d404238
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-19 19:09:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word (string)
   
   array_of_strings = string.split("")
   
-  array_of_strings.each do |string| string.reverse
-  array_of_strings
+  array_of_strings.each do |string| string.reverse!
+  
 end
   
   array_of_strings.join('')
